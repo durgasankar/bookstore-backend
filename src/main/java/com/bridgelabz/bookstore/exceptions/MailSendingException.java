9c3f025fd7892790fa0ff09b753c57e16e7a6f1b
durgasankar <durgasankar.raja500@gmail.com>
2020-04-12 20:08:05 +0530

    [durgasankar].add -> email sending configuration file added which uses rabbit mq for sending message

---
+++ src/main/java/com/bridgelabz/bookstore/exceptions/MailSendingException.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.bookstore.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * This class extends {@link RuntimeException} and creates a custom exception
+ * which would be thrown during the user's abnormal activity while performing
+ * mail sending operations.
+ * 
+ * @author Durgasankar Mishra
+ * @created 2020-04-12
+ * @version 1.0
+ */
+public class MailSendingException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private final HttpStatus httpStatus;
+
+	/**
+	 * Constructor takes message and Status code as input parameter and fetch
+	 * message from its superclass.
+	 *
+	 * @param message    as String input parameter
+	 * @param httpStatus as Integer input parameter
+	 */
+	public MailSendingException( String message, HttpStatus httpStatus ) {
+		super( message );
+		this.httpStatus = httpStatus;
+	}
+
+}
---
+++ src/main/java/com/bridgelabz/bookstore/utility/Util.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.bookstore.utility;
+
+public class Util {
+
+    private Util(){
+
+    }
+
+    public static final String SENDER_EMAIL_ID = "jc100.r20jc134@gmail.com";
+    public static final String SENDER_PASSWORD = "r20jc134";
+
+}
